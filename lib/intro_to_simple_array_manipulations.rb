0c69fae901adb24f5b9b5fca1ed8dd7b3612e4ba
Sung Bae <singsang2@gmail.com>
2018-10-06 20:23:07 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push(array, element)
+  array.push(element)
+end
+
+def using_unshift(array, element)
+  array.unshift(element)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  array.pop
+  array.pop
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift()
+  array.shift()
+end
+
+def using_concat(array1, array2)
+  conc
